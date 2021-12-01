e1ca54468b91a027ba294d4c1470b57c77741959
rakasan <racasan.vlad@gmail.com>
2021-12-01 23:54:04 +0200

    basic control flow app
    Just a test for detecting if a number is positive or negative

---
+++ 4_BasicControlFlow/basic_ControlFlow.cpp
@@ -0,0 +1,23 @@
+/* Simple app to verify if the inserted number is positive or not */
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int number;
+cout << "Enter the number ";
+cin >>number;
+
+/* Check the sense */
+    if(number >= 0)
+    {
+        cout <<"The number is positive";
+    }
+    else
+    {
+
+        cout <<"The number is negative";
+    }
+
+return 0;
+}
