37afb3de6e1f7a008fc7b241df6eba591df8a153
rakasan <racasan.vlad@gmail.com>
2021-12-01 23:04:45 +0200

    Added a short application for data size
    Created a simple application for showing the size of all data types

---
+++ 2_DataTypes_size/size_of_datatypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("Size of  char %d bytes \n",sizeof(char));
+    printf("Size of  int %d bytes \n",sizeof(int));
+    printf("Size of  short int %d bytes \n",sizeof(short int));
+    printf("Size of  long int %d bytes \n",sizeof(long int));
+    printf("Size of  signed long int %d bytes \n",sizeof(signed long int));
+    printf("Size of  unsigned long int %d bytes \n",sizeof(unsigned long int));
+    printf("Size of  float %d bytes \n",sizeof(float));
+    printf("Size of  double %d bytes \n",sizeof(double));
+    printf("Size of  w_char %d bytes \n",sizeof(wchar_t));
+
+    return 0;
+
+
+}
